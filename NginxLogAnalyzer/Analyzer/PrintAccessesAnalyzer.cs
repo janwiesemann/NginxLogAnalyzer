381f7a6513cdfdde570c625c8288a1eb1f7ba377
Jan  Wiesemann <kontakt@janwiesemann.de>
2021-08-08 19:05:39 +0200

    print accesses analyser

---
+++ NginxLogAnalyzer/Analyzer/PrintAccessesAnalyzer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NginxLogAnalyzer.Settings;
+
+namespace NginxLogAnalyzer.Analyzer
+{
+    internal class PrintAccessesAnalyzer : IAnalyzer
+    {
+        public string Name => "Accesses";
+
+        public bool CanExecute(IEnumerable<char> switches)
+        {
+            return switches.HasSwitch('r');
+        }
+
+        public void Execute(IEnumerable<RemoteAddress> addresses, IEnumerable<ISetting> settings)
+        {
+            settings.GetCountSettings(out int addressCount, out int entryCount);
+
+            List<KeyValuePair<AccessEntry, RemoteAddress>> allItems = new List<KeyValuePair<AccessEntry, RemoteAddress>>();
+            foreach (RemoteAddress address in addresses)
+            {
+                foreach (AccessEntry entry in address.AccessEntrys)
+                    allItems.Add(new KeyValuePair<AccessEntry, RemoteAddress>(entry, address));
+            }
+
+            foreach (KeyValuePair<AccessEntry, RemoteAddress> entry in allItems.OrderByDescending(i => i.Key.DateTime))
+            {
+                Console.WriteLine($"{entry.Value.Address.PadRight(4 * 3 + 3)}: {entry.Key.DateTime} => {entry.Key.Request?.ToString()}");
+
+                if (!Count.Continue(ref addressCount))
+                    break;
+            }
+        }
+    }
+}
