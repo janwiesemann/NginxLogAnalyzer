87016178bec17a2c0508221929806186a45e1d6b
Jan  Wiesemann <kontakt@janwiesemann.de>
2021-08-08 19:58:17 +0200

    fixed date parsing

--- NginxLogAnalyzer/AccessLogParser.cs
+++ NginxLogAnalyzer/AccessLogParser.cs
@@ -44,14 +44,71 @@ namespace NginxLogAnalyzer
 
         private static string ParseBlock(string line, ref int offset) => ReadWhile(line, ref offset, (c, sb) => c != ' ' && c != '\t');
 
+        private static int ParseNumber(string line, ref int offset)
+        {
+            string str = ReadWhile(line, ref offset, (c, sb) => char.IsNumber(c));
+
+            return int.Parse(str);
+        }
+
+        private static int ParseMonth(string line, ref int offset)
+        {
+            string str = ReadWhile(line, ref offset, (c, sb) => c != '/');
+            str = str.ToUpper();
+
+            if (str == "JAN")
+                return 1;
+
+            if (str == "FEB")
+                return 2;
+
+            if (str == "MAR")
+                return 3;
+
+            if (str == "APR")
+                return 4;
+
+            if (str == "MAY")
+                return 5;
+
+            if (str == "JUN")
+                return 6;
+
+            if (str == "JUL")
+                return 7;
+
+            if (str == "AUG")
+                return 8;
+
+            if (str == "SEP" || str == "SEPT")
+                return 9;
+
+            if (str == "OCT")
+                return 10;
+
+            if (str == "NOV")
+                return 11;
+
+            if (str == "DEC")
+                return 12;
+
+            return 1;
+        }
+
         private static DateTime ParseDateTime(string line, ref int offset)
         {
             ReadWhile(line, ref offset, (c, sb) => c != '[');
 
-            string dateString = ReadWhile(line, ref offset, (c, sb) => c != ']');
-            DateTime.TryParseExact(dateString, "dd/MMM/yyyy:hh:mm:ss K", null, DateTimeStyles.None, out DateTime res);
+            int day = ParseNumber(line, ref offset);
+            int month = ParseMonth(line, ref offset);
+            int year = ParseNumber(line, ref offset);
+            int hour = ParseNumber(line, ref offset);
+            int minute = ParseNumber(line, ref offset);
+            int second = ParseNumber(line, ref offset);
 
-            return res;
+            ReadWhile(line, ref offset, (c, sb) => c != ']');
+
+            return new DateTime(year, month, day, hour, minute, second);
         }
 
         private static Request ParseRequest(string line, ref int offset)
